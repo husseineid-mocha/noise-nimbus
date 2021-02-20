694d685cadd57d6886f35ce43042809a5857c10e
Hussein Eid <husseineid@mochamacbook.lan>
2021-02-20 14:03:32 -0500

    splash 2

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
+import logo from "./soundcloud-512.png";
+
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Splash() {
+  //   const sessionUser = useSelector((state) => state.session.user);
+
   return (
     <div className="splash">
       <div className="splash-top-line"></div>
       <div className="splash-banner">
-        <div className="splash-banner navbar"></div>
+        <div className="splash-banner-navbar">
+          <div className="splash-banner-left">
+            <img src={logo}></img>
+          </div>
+
+          <div className="splash-banner-right">
+            <LoginFormModal className="splash-login" />
+            <SignupFormModal className="splash-signup" />
+          </div>
+        </div>
       </div>
     </div>
   );
